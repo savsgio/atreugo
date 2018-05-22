37c3bb2344d5675ab45db4afa40ba24493fd3434
Sergio Andres Virviescas Santana <savirviescas@qdqmedia.com>
2018-05-22 12:20:41 +0200

    Added jwt auth middleware example and util to convert bytes to string without memory allocation

---
+++ examples/jwt_auth_middleware_sever.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/erikdubbelboer/fasthttp"
+	"github.com/savsgio/atreugo"
+	"github.com/savsgio/go-logger"
+)
+
+var jwtSignKey = []byte("TestForFasthttpWithJWT")
+
+type userCredential struct {
+	Username []byte `json:"username"`
+	Password []byte `json:"password"`
+	jwt.StandardClaims
+}
+
+func init() {
+	logger.Setup(logger.DEBUG)
+}
+
+func generateToken(username []byte, password []byte) (string, time.Time) {
+	logger.Debugf("Create new token for user %s", username)
+
+	expireAt := time.Now().Add(1 * time.Minute)
+
+	// Embed User information to `token`
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS512, &userCredential{
+		Username: username,
+		Password: password,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireAt.Unix(),
+		},
+	})
+
+	// token -> string. Only server knows the secret.
+	tokenString, err := newToken.SignedString(jwtSignKey)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	return tokenString, expireAt
+}
+
+func validateToken(requestToken string) (*jwt.Token, *userCredential, error) {
+	logger.Debug("Validating token...")
+
+	user := &userCredential{}
+	token, err := jwt.ParseWithClaims(requestToken, user, func(token *jwt.Token) (interface{}, error) {
+		return jwtSignKey, nil
+	})
+
+	return token, user, err
+}
+
+// checkTokenMiddleware middleware to check jwt token authoritation
+func checkTokenMiddleware(ctx *fasthttp.RequestCtx) (int, error) {
+	// Avoid middleware when you are going to login view
+	if atreugo.B2S(ctx.Path()) == "/login" {
+		return fasthttp.StatusOK, nil
+	}
+
+	jwtCookie := ctx.Request.Header.Cookie("atreugo_jwt")
+
+	if len(jwtCookie) == 0 {
+		return fasthttp.StatusForbidden, errors.New("login required")
+	}
+
+	token, _, err := validateToken(atreugo.B2S(jwtCookie))
+
+	if !token.Valid {
+		return fasthttp.StatusForbidden, errors.New("your session is expired, login again please")
+	}
+
+	return fasthttp.StatusOK, err
+}
+
+func main() {
+	server := atreugo.New()
+
+	server.UseMiddleware(checkTokenMiddleware)
+
+	server.Path("GET", "/", func(ctx *fasthttp.RequestCtx) error {
+		return atreugo.HttpResponse(ctx,
+			[]byte(fmt.Sprintf(`<h1>You are login with JWT</h1>
+				JWT cookie value: %s`, ctx.Request.Header.Cookie("atreugo_jwt"))))
+	})
+
+	server.Path("GET", "/login", func(ctx *fasthttp.RequestCtx) error {
+		qUser := []byte("savsgio")
+		qPasswd := []byte("mypasswd")
+
+		jwtCookie := ctx.Request.Header.Cookie("atreugo_jwt")
+
+		if len(jwtCookie) == 0 {
+			tokenString, expireAt := generateToken(qUser, qPasswd)
+
+			// Set cookie for domain
+			cookie := fasthttp.AcquireCookie()
+			cookie.SetKey("atreugo_jwt")
+			cookie.SetValue(tokenString)
+			cookie.SetExpire(expireAt)
+			ctx.Response.Header.SetCookie(cookie)
+		}
+
+		ctx.Redirect("/", ctx.Response.StatusCode())
+
+		return nil
+	})
+
+	server.ListenAndServe("0.0.0.0", 8000)
+}
