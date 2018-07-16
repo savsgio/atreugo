a3a25198e049a1f589c02d7e3bc363beac091e45
Sergio Andres Virviescas Santana <savirviescas@qdqmedia.com>
2018-07-16 12:49:13 +0200

    Fix doc.go

--- doc.go
+++ doc.go
@@ -1,8 +1,5 @@
-/*
-Micro-framework to make simple the use of routing and middleware
-with all optimizations of fasthttp
-
-This micro-framework is build on top erikdubbelboer's fasthttp fork.
-*/
-
+// Package atreugo is a micro-framework to make simple the use of routing and middlewares
+// with all optimizations of fasthttp
+//
+// This micro-framework is build on top erikdubbelboer's fasthttp fork.
 package atreugo
