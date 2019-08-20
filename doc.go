2a32625c83cbfdef25aa9da8485d02958533d78d
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2019-08-20 10:14:52 +0200

    Fix doc.go

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@ Package atreugo is a high performance and extensible micro web framework with ze
 It's build on top of fasthttp and provides the following features:
 
     * Optimized for speed. Easily handles more than 100K qps and more than 1M
-      concurrent keep-alive connections on modern hardware.
+        concurrent keep-alive connections on modern hardware.
     * Optimized for low memory usage.
     * Easy 'Connection: Upgrade' support via RequestCtx.Hijack.
     * Server provides the following anti-DoS limits:
@@ -27,32 +27,32 @@ It's build on top of fasthttp and provides the following features:
         * Unique request id.
         * Request start time.
         * Connection start time.
-		* Request sequence number for the current connection.
+        * Request sequence number for the current connection.
 
-	* Middlewares support:
+    * Middlewares support:
 
-		* Before view execution.
+        * Before view execution.
         * After view execution.
 
-	* Easy routing:
+    * Easy routing:
 
-		* Path parameters (mandatories and optionals).
-		* Views with timeout.
+        * Path parameters (mandatories and optionals).
+        * Views with timeout.
         * Group paths and middlewares.
-		* Static files.
-		* Serve one file like pdf, etc.
-		* Filters (middlewares) to especific views.
+        * Static files.
+        * Serve one file like pdf, etc.
+        * Filters (middlewares) to especific views.
         * net/http handlers support.
         * fasthttp handlers support
 
-	* Common responses (also you could use your own responses):
+    * Common responses (also you could use your own responses):
 
-		* JSON
-		* HTTP
-		* Text
-		* Raw
-		* File
-		* Redirect
+        * JSON
+        * HTTP
+        * Text
+        * Raw
+        * File
+        * Redirect
 
 */
 package atreugo
