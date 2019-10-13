68baf07f6b8827fc7f3f68d41f9182178c33e9c6
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2019-10-13 18:16:51 +0200

    Remove unsed utils functions

--- utils.go
+++ utils.go
@@ -2,23 +2,6 @@ package atreugo
 
 import "github.com/valyala/fasthttp"
 
-// index returns the first index of the target string `t`, or
-// -1 if no match is found.
-func indexOf(vs []string, t string) int {
-	for i, v := range vs {
-		if v == t {
-			return i
-		}
-	}
-	return -1
-}
-
-// include returns `true` if the target string t is in the
-// slice.
-func include(vs []string, t string) bool {
-	return indexOf(vs, t) >= 0
-}
-
 // execute executes all middlewares + filters + view with the given request context
 func execute(ctx *RequestCtx, hs []Middleware) error {
 	for _, h := range hs {
